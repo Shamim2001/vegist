881a883f180b161d3597a7adc30496fef7bb67db
Shamim2001 <shamimahmedridoy2016@gmail.com>
2023-07-11 23:44:38 +0600

    cart increment and decrement error solve

--- resources/views/frontend/cart/index.blade.php
+++ resources/views/frontend/cart/index.blade.php
@@ -1,13 +1,15 @@
 @extends('frontend.layouts.master')
 
-@section('title', 'Vegist eCommerce')
+@section('title', 'Cart')
 
 @section('content')
 
-    <!-- breadcrumb start -->
     <x-frontend.breadcrumb title="Cart" />
-    <!-- breadcrumb end -->
-    @php $total = null  @endphp
+
+    @php $total = null @endphp
+
+
+
     <!-- cart start -->
     <section class="cart-page section-tb-padding">
         <div class="container">
@@ -17,27 +19,32 @@
                         <div class="cart-details">
                             <div class="cart-item">
                                 <span class="cart-head">My cart:</span>
-                                <span class="c-items">{{ count($carts) }} items</span>
+                                <span class="c-items">{{ count($carts) }} item</span>
                             </div>
-                            @forelse ($carts as $cart)
-                                @if (Cookie::get('shopping_cart'))
-                                    @php
-                                        $total += $cart->item_quantity * $cart->item_price;
-                                    @endphp
-                                @endif
+                        </div>
+                    </div>
+
+                    @forelse ($carts as $cart)
+                        @if (Cookie::get('shopping_cart'))
+                            @php $total += ($cart->item_quantity * $cart->item_price) @endphp
+                        @endif
+                        <div class="cart-area">
+                            <div class="cart-details">
                                 <div class="cart-all-pro">
                                     <div class="cart-pro">
                                         <div class="cart-pro-image">
-                                            <a href="{{ route('front.shop.single', $cart->item_slug) }}"><img width="100"
-                                                    src="{{ $cart->item_image }}" class="img-fluid" alt="image"
-                                                    style="width: 200px;"></a>
+                                            <a href="{{ route('front.shop.single', $cart->item_slug) }}"><img
+                                                    src="{{ getAssetUrl($cart->item_image) }}" class="img-fluid"
+                                                    width="80" alt="{{ $cart->item_name }}"></a>
                                         </div>
                                         <div class="pro-details">
                                             <h4><a
                                                     href="{{ route('front.shop.single', $cart->item_slug) }}">{{ $cart->item_name }}</a>
                                             </h4>
-                                            <span class="cart-pro-price">${{ number_format($cart->item_price, 2) }}
-                                                USD</span>
+
+                                            <span class="cart-pro-price">${{ number_format($cart->item_price, 2) }}</span>
+
+
                                         </div>
                                     </div>
                                     <div class="qty-item">
@@ -57,19 +64,18 @@
                                     </div>
                                     <div class="all-pro-price">
                                         <span
-                                            class="item-total">${{ number_format($cart->item_quantity * $cart->item_price, 2) }}
-                                            USD</span>
-                                        <span class="ms-3">
-                                            <a href="javascript:void(0)" data-id="{{ $cart->item_id }}" class="text-danger remove-cart-item"><i
-                                                    class="fa fa-remove"></i></a>
-                                        </span>
+                                            class='item-total'>${{ number_format($cart->item_quantity * $cart->item_price, 2) }}</span>
+                                        <span class="ms-3"><a href="javascript:void(0)" data-id="{{ $cart->item_id }}"
+                                                class="text-danger remove-cart-item"><i class="fa fa-remove"></i></a></span>
                                     </div>
                                 </div>
-                            @empty
-                                <div class="text-center text-danger">No Item Found!</div>
-                            @endforelse
+                            </div>
                         </div>
-                    </div>
+                    @empty
+                        <div class="text-center">No item found!</div>
+                    @endforelse
+
+
                 </div>
                 <div class="col-xl-3 col-xs-12 col-sm-12 col-md-12 col-lg-4">
                     <div class="cart-total">
@@ -81,11 +87,12 @@
                             <span>Discount</span>
                             <span class="discount">$0.00 USD</span>
                         </div>
+
                         <div class="shop-total">
                             <span>Total</span>
                             <span class="total-amount">${{ $total ? number_format($total, 2) : 0 }} USD</span>
                         </div>
-                        <a href="javascript:void(0)" class="check-link btn btn-style1">Checkout</a>
+                        <a href="javascript:voic(0)" class="check-link btn btn-style1">Checkout</a>
                     </div>
                 </div>
             </div>
@@ -93,32 +100,37 @@
     </section>
     <!-- cart end -->
 
+
 @endsection
+
 
 @push('js')
     <script>
-        // Increment
+        // Decrement
         $(document).on('click', '.minus-btn', function(e) {
             e.preventDefault();
             var el = $(this).closest('.cart-area');
             var product_id = $(this).data('id');
-
             updateCart('minus', el, product_id);
-        })
+        });
 
-        // Decrement
+        // Increment
         $(document).on('click', '.plus-btn', function(e) {
             e.preventDefault();
             var el = $(this).closest('.cart-area');
             var product_id = $(this).data('id');
             updateCart('plus', el, product_id);
-        })
 
-        // Update cart
+        });
+
+
+        // Update Cart
         function updateCart(method, el, product_id) {
-            // Item Details
+
+            // Item details
             var quantity = el.find('.quantity').val();
-            var price = parseFloat(el.find('.cart-pro-price').html().replace(/[^0-9.-]+/g, ""));
+            var price = parseFloat(el.find('.cart-pro-price').html().replace(
+                /[^0-9.-]+/g, ""));
 
             var itemTotal = quantity * price;
 
@@ -144,25 +156,23 @@
 
             var sumWithDiscount = sum - discount;
 
-            // format the sum as a currency amount string
+            // Format the sum as a currency amount string
             var sumString = '$' + sum.toLocaleString('en-US', {
                 style: 'decimal',
                 minimumFractionDigits: 2
             }) + " USD";
-
             var sumWithDiscountString = '$' + sumWithDiscount.toLocaleString('en-US', {
                 style: 'decimal',
                 minimumFractionDigits: 2
             }) + " USD";
 
-
-            // update Data
+            // update data
             el.find('span.item-total').html(itemTotalString);
             $('span.total').html(sumString);
             $('span.total-amount').html(sumWithDiscountString);
 
 
-            // Ajag
+            // Ajax
             $.ajax({
                 url: '{{ route('front.cart.store') }}',
                 method: "POST",
